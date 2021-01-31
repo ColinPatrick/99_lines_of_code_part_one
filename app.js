c9569cd649900d959cc19969caae9a07eae41514
Colin Huckestein <cphuckestein@gmail.com>
2021-01-31 16:48:02 -0600

    first commit

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
-let friends = ['Elliot', 'Darlene', 'Angela', 'Tyrell', 'Dom'];
-var count = 99;
-var word = "lines";
+window.addEventListener('DOMContentLoaded', function () {
+  let friends = ['Elliot', 'Darlene', 'Angela', 'Tyrell', 'Dom'];
+  var count = 99;
+  var word = "lines";
+  let singBtn = document.getElementById('singBtn');
 
-for (let n = 0; n < friends.length; n++) {
-  console.log(friends[n] + ':');
-  while (count > 0) {
-    if (count == 1) {
-      word = "line";
-    }
-    console.log(count + ' ' + word + ' of code in the file, ' + count + ' ' + word + ' of code;');
-    console.log(friends[n] + ' strikes one out, clears it all out,');
-    count = count - 1;
-    if (count == 1) {
-      word = "line";
-    } else {
-      word = "lines";
-    }
-    console.log(count + ' ' + word + ' of code on the wall.');
-  }
-  count = 99;
-}
+  singBtn.addEventListener('click', function () {
+    for (let n = 0; n < friends.length; n++) {
+      let friendArea = document.createElement('div');
+      friendArea.className = 'friend';
+      let friendHeader = document.createElement('h3');
+      let friendName = document.createTextNode(friends[n] + ':');
+
+      friendHeader.appendChild(friendName);
+      friendArea.appendChild(friendHeader);
+      document.body.appendChild(friendArea);
+
+      while (count > 0) {
+        if (count == 1) {
+          word = "line";
+        }
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+
+        let p1Text = document.createTextNode(count + ' ' + word + ' of code in the file, ' + count + ' ' + word + ' of code;');
+        let p2Text = document.createTextNode(friends[n] + ' strikes one out, clears it all out,');
+
+        p1.appendChild(p1Text);
+        p2.appendChild(p2Text);
+        friendArea.appendChild(p1);
+        friendArea.appendChild(p2);
+
+        count = count - 1;
+        if (count == 1) {
+          word = "line";
+        } else {
+          word = "lines";
+        }
+
+        let p3Text = document.createTextNode(count + ' ' + word + ' of code on the wall.');
+
+        p3.appendChild(p3Text);
+        friendArea.appendChild(p3);
+
+      }
+      count = 99;
+    };
+  });
+
+});
